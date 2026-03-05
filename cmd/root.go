581f7c09b5ebaf3162586cea5e4a43125ee80f38
Canh Ta <canhta.w@gmail.com>
2026-03-05 22:18:01 +0700

    feat: add version injection via ldflags

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var appVersion string
+
 var rootCmd = &cobra.Command{
 	Use:   "foreman",
 	Short: "Autonomous software development daemon",
 	Long:  "An autonomous coding daemon that turns issue tracker tickets into tested, reviewed pull requests.",
+}
+
+func SetVersion(v string) {
+	appVersion = v
+	rootCmd.Version = v
 }
 
 func Execute() error {
